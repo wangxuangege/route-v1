7fca09c023657b30f1c83a0e74ffb33cac0f393b
xqhuang <xqhuang@chinaums.com>
2018-06-07 11:57:24 +0800

    简单的路由判断逻辑，不考虑性能，每次直接从mysql读取规则，然后计算规则，后期会考虑将规则放缓存、本地缓存等优化性能，目前先不考虑，只考虑流程

--- src/route/Route.lua
+++ src/route/Route.lua
@@ -5,6 +5,8 @@ local LogUtil = require("util.LogUtil")
 local CONFIG = require("constant.Config")
 local ERR_CODE = require("constant.ErrCode")
 local RouteContext = require("route.RouteContext")
+local Mysql = require("respository.mysql.Mysql")
+local StringUtil = require("util.StringUtil")
 
 local log = LogUtil:new("ROUTE")
 
@@ -12,7 +14,7 @@ local log = LogUtil:new("ROUTE")
 -- 处理结果处理
 --------------------------------------------------------------------------------------
 local function handleRet(ngx, upstream)
-    if not ngx then
+    if not ngx or ngx.__TEST__ then
         log:info("路由计算结果为：", upstream)
         return
     end
@@ -21,18 +23,101 @@ local function handleRet(ngx, upstream)
 end
 
 --------------------------------------------------------------------------------------
+-- 所有允许的规则类型
+--------------------------------------------------------------------------------------
+local function allowRules()
+    return {
+        IP_RANGE = require("rule.IpRangeRule"),
+        PARAM_TAIL = require("rule.ParamTailRule")
+    }
+end
+
+--------------------------------------------------------------------------------------
+-- 查询所有规则
+-- @return code detail
+--------------------------------------------------------------------------------------
+local function queryAllRules()
+    -- 允许的规则类型
+    local ruleTypes = allowRules()
+    if not next(ruleTypes) then
+        return {}
+    end
+
+    local mysql = Mysql:create()
+    local code, detail = mysql:query("select * from route_rule where status='OPEN'")
+    if code ~= ERR_CODE.SUCCESS then
+        return code, detail
+    end
+    mysql:release()
+
+    local result = {}
+
+    -- 转换为规则对象
+    for i = 1, #detail do
+        local row = detail[i]
+        local type = row.rule_type
+        local ruleType = ruleTypes[type]
+        if ruleType then
+            local rules_str = row.rules_str
+            local priority = tonumber(row.priority)
+            local rule = ruleType:new(rules_str, priority)
+            if rule.effective then
+                table.insert(result, rule)
+            else
+                log:warn("忽略无效规则,rowId=", row.id)
+            end
+        else
+            log:warn("忽略无效规则,rowId=", row.id)
+        end
+    end
+
+    -- 按照优先级排序
+    table.sort(result, function(left, right)
+        return left.priority > right.priority
+    end)
+
+    return ERR_CODE.SUCCESS, result
+end
+
+--------------------------------------------------------------------------------------
 -- 判断路由入口
 -- @param ngx
 -- @return code detail
 --------------------------------------------------------------------------------------
 local function route(ngx)
-    local context = RouteContext:build2(ngx)
-    local result = "BETA1"
+    local result = CONFIG.DEFAULT_UPSTREAM
+    local hitRule = nil
+
+    -- 目前先不考虑性能，直接从DB获取所有规则，后期优化
+    local code, detail = queryAllRules()
+    if code == ERR_CODE.SUCCESS then
+        local context = RouteContext:build(ngx)
+        for i = 1, #detail do
+            local rule = detail[i]
+            local code, detail = rule:getUpstream(context)
+            if code == ERR_CODE.SUCCESS then
+                result = detail.upstream
+                hitRule = detail.rule
+                break
+            end
+        end
+    end
+
+    if hitRule ~= nil then
+        log:info("命中规则：", StringUtil.toJSONString(hitRule))
+    else
+        log:info("没有命中任何规则，走默认规则")
+    end
+
     handleRet(ngx, result)
 end
 
-
 if CONFIG.DEBUG then
+    local ngx = {
+        __TEST__ = {
+            ip = "172.168.1.3"
+        }
+    }
     -- DEBUG模式下面直接抛出异常
     route(ngx)
 else

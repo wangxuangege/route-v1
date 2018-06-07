c4131e9c62d2a1e4a1d6c94bab548776f5e25c08
xqhuang <xqhuang@chinaums.com>
2018-06-07 21:10:55 +0800

    删除规则后台逻辑编写

--- src/admin/DeleteCommand.lua
+++ src/admin/DeleteCommand.lua
@@ -1,5 +1,12 @@
 module(..., package.seeall);
 
+local StringUtil = require("util.StringUtil")
+local ERR_CODE = require("constant.ErrCode")
+local Mysql = require("respository.mysql.Mysql")
+local LogUtil = require("util.LogUtil")
+
+local log = LogUtil:new("DELETE")
+
 --------------------------------------------------------------------------------------
 -- 执行命令
 --------------------------------------------------------------------------------------
@@ -24,11 +31,14 @@ end
 --------------------------------------------------------------------------------------
 function checkParams(requestParams)
     local id = requestParams['id']
+
     if StringUtil.isEmpty(id) then
         return ERR_CODE.ADMIN_PARAM_ERROR, '必须指定要删除的规则主键'
     end
+
     if not tonumber(id) then
         return ERR_CODE.ADMIN_PARAM_ERROR, '找不到要删除的主键'
     end
+
     return ERR_CODE.SUCCESS
 end

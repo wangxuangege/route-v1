a6f34da1560ef9cab0f1c20d548d488ffef8478b
xqhuang <xqhuang@chinaums.com>
2018-06-07 16:40:32 +0800

    前端页面调整

--- route-admin/dist/js/admin.js
+++ route-admin/dist/js/admin.js
@@ -3,7 +3,7 @@
  *
  * @type {string}
  */
-var ADMIN_URL = "http://192.168.171.135:8080/admin";
+var ADMIN_URL = "http://192.168.171.131:8080/admin";
 
 /**
  * 字符串buffer类
@@ -20,23 +20,4 @@ StringBuffer.prototype.append = function (str) {
 }
 StringBuffer.prototype.toString = function () {
     return this.__strings__.join("");
-}
-
-
-/**
- * 加载规则数据
- *
- * @param loadSuccess 加载数据成功后回调方法
- */
-function loadRouteRules(loadSuccess) {
-    $.getJSON(ADMIN_URL, {
-            command: 'QUERY',
-            type: 'ALL'
-        },
-        function (result) {
-            if (result.success) {
-                loadSuccess(result.data)
-            }
-        }
-    );
 }
